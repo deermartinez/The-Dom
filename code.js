25428556a12ad4be589a0a6d6bc421349a216c40
Destynee Martine <deermartinez8@gmail.com>
2021-01-14 19:15:13 -0700

    replace favorite

--- code.js
+++ code.js
@@ -4,3 +4,9 @@
 // (In the JavaScript) Change the body tag's style so it has a font-family of "Arial, sans-serif".
 document.body.style.fontFamily = "Arial,sans-serif";
 
+
+// (In the JavaScript) Replace each of the spans (nickname, favorites, hometown) with your own information.
+document.getElementById('nickname').textContent = 'Kev';
+document.getElementById('favorites')
+
+
