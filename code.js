8f7f88dc062292d5cf478328813afba209408f20
Destynee Martine <deermartinez8@gmail.com>
2021-01-14 19:12:51 -0700

    style

--- code.js
+++ code.js
@@ -1,3 +1,6 @@
 
 
-Document.body.style.fontfamily = “Arial, sans-serif”;
+// <!-- Add a script tag to the bottom of the HTML body.
+// (In the JavaScript) Change the body tag's style so it has a font-family of "Arial, sans-serif".
+document.body.style.fontFamily = "Arial,sans-serif";
+
