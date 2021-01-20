953cc7256abcefede8ed96ab9c6dfc88db7c541d
Destynee Martine <deermartinez8@gmail.com>
2021-01-19 20:17:45 -0700

    changed file

---
+++ forms.js
@@ -0,0 +1,9 @@
+// //a function to display the error message
+// function printError(elemId, hintMsg){
+//     document.getElementById(elemId).innerHTML=hintMsg;
+// }
+
+
+// //function to validate the form
+// function validateForm
+// //want to target all elements on form
