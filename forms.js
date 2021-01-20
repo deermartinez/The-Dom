6bc78ef5fdc3263bf19dd993dde42dd315416009
Destynee Martine <deermartinez8@gmail.com>
2021-01-19 20:45:33 -0700

    validate

--- forms.js
+++ forms.js
@@ -1,9 +1,50 @@
-// //a function to display the error message
-// function printError(elemId, hintMsg){
-//     document.getElementById(elemId).innerHTML=hintMsg;
-// }
+//a function to display the error message
+function printError(elemId, hintMsg){
+    document.getElementById(elemId).innerHTML=hintMsg;
+}
 
 
-// //function to validate the form
-// function validateForm
-// //want to target all elements on form
+//function to validate the form
+function validateForm(){
+//want to target all elements on form
+var name = document.contactForm.name.value;
+
+var email=document.contactForm.email.value;
+var mobile=document.contactForm.mobile.value;
+var country=document.contactForm.country.value;
+var gender=document.contactForm.gender.value;
+var hobbies=[];
+var checkboxes=document.getElementsByTagName('hobbies[]');
+//hobbies is different ecause it was checkboxes-not all would be chosen
+
+//now we want to push up hobbies or the selected hobbies into an empty array
+for (let i=0; i<checkboxes.length; i++){
+    if(checkboxes[i].checked){//[i] because it is an array
+        //.checked obviously means it was checked/selected
+    hobbies.push(checkboxes[i].value);
+    }
+    }
+}
+
+
+//
+var nameErr=emailErr=mobileErr=countryErr=genderErr=false;
+if (name==" "){//if name is empty print error
+    printError ("nameErr", "Please enter a valid name")
+}else{
+    var regex= /^[a-zA-Z\s]+$/;
+    if(regex.test(name)=== false){
+        //regrex is pattern attribute
+        //if not matching pattern, print error
+        printError("nameErr", "Please enter a valid Name");
+    } else {
+        printError("nameErr", "")
+        nameErr = false;
+    }
+}
+
+
+
+
+//validate email
+
