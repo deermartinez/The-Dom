1c388d76d4b2cfdc0c0e15311fa83eb56fc2ef15
Destynee Martine <deermartinez8@gmail.com>
2021-02-09 10:49:28 -0700

    correction

--- libs.js
+++ libs.js
@@ -1,16 +1,13 @@
 //create a function for madlibs
 function libIts(){
     var storyDiv = document.getElementById('story');
-    var person = document.getElementById('person');
-    var adjective = document.getElementById('adjective');
+    var person = document.getElementById('person').value;
+    var adjective = document.getElementById('adjective').value;
     var noun = document.getElementById('noun').value;
     //put together as a story
-    storyDiv.innerHTML = person + "is locked down" + adjective + " " + noun + "Wow this is great ";
-
-
-
+    // storyDiv.innerHTML = person + "is locked down" + adjective + " in an underwater  " + noun + "Wow this is great ";
+    storyDiv.innerHTML = person + adjective + noun;
 }
-
 
 var libButton = document.getElementById('lib-button');
 libButton.addEventListener('click', libIts);
